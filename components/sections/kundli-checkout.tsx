844de22487ea642c44ba3101866807257f35f805
Your Name <tradexbusiness78@gmail.com>
2026-04-13 04:40:57 +0530

    Fix checkout dataLayer type conflict

--- components/sections/kundli-checkout.tsx
+++ components/sections/kundli-checkout.tsx
@@ -24,7 +24,6 @@ declare global {
   interface Window {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     Razorpay: any;
-    dataLayer?: Array<Record<string, unknown>>;
   }
 }
 
